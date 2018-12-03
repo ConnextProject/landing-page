7656d5df5f346df86fd412e10dfaa5e7b1d6d480
Hunter Hillman <hthillman@Hunters-MacBook-Pro.local>
2018-12-03 12:05:39 -0500

    routing fixes

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import nothing_to_see_here from "../assets/nothing_to_see_here.gif";
-import "./NotFound.css"
+import {Link} from "react-router-dom";
+import "./NotFound.css";
 
 class NotFound extends Component{
     render() {
@@ -11,12 +12,12 @@ class NotFound extends Component{
                 <h1 style={{fontFamily:"Comfortaa", fontSize:"80px", color:"#FFF"}}>404</h1>
                 <h1 style={{fontFamily:"Comfortaa", marginTop:"25%"}}> Oops! This page doesn't exist. Maybe you were looking for one of these?</h1>
                 <div className="buttons" style={{display:"flex"}}>
-                    <a className="home" href="https://connext.network" style={{marginTop:"5%",marginBottom:"5%",backgroundColor:"#FFF",textDecoration:"none"}}>
+                    <Link className="home" to="/" style={{marginTop:"5%",marginBottom:"5%",backgroundColor:"#FFF",textDecoration:"none"}}>
                         Home
-                    </a>
-                    <a className="about" href="https://connext.network/team" style={{backgroundColor:"#FFF",textDecoration:"none",color:"#FFF"}}>
+                    </Link>
+                    <Link className="about" to="/Team" style={{backgroundColor:"#FFF",textDecoration:"none",color:"#FFF"}}>
                         About
-                    </a>
+                    </Link>
                 </div>
                 <img className="gif" src={nothing_to_see_here} alt="Please disperse gif" style={{borderRadius:"8px"}}/>
             </div>
