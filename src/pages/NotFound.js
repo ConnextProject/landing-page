ea98fedc51c1cafcf80fbd5ef39d243ae7208d72
Hunter Hillman <hthillman@Hunters-MacBook-Pro.local>
2018-12-10 09:47:06 -0500

    large screen spacing fixes

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -10,7 +10,7 @@ class NotFound extends Component{
                             height:"100%",
                             alignItems:"center"}}>
                 <h1 style={{fontFamily:"Comfortaa", fontSize:"80px", color:"#FFF"}}>404</h1>
-                <h1 style={{fontFamily:"Comfortaa", marginTop:"25%"}}> Oops! This page doesn't exist. Maybe you were looking for one of these?</h1>
+                <h1 style={{fontFamily:"Comfortaa", marginTop:"10%"}}> Oops! This page doesn't exist. Maybe you were looking for one of these?</h1>
                 <div className="buttons" style={{display:"flex"}}>
                     <Link className="home" to="/" style={{marginTop:"5%",marginBottom:"5%",backgroundColor:"#FFF",textDecoration:"none"}}>
                         Home
