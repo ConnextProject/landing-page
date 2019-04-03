66c7befb42a0563fbe6aff02da084048543c23a8
Bo Henderson <bohende@gmail.com>
2019-04-03 17:07:49 +0530

    install before building before deploying

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -8,6 +8,7 @@ echo "Deploying $static_folder contents to s3 bucket: $bucket_name"
 sleep 1
 
 # Make sure the resources we're about to deploy are up-to-date
+npm install
 npm run build
 
 if [[ -n "$AWS_ACCESS_KEY_ID" || -n "$AWS_SECRET_ACCESS_KEY" ]]
